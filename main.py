4664643d252a262a0b45a468ac76d20accd55c5b
kronenz <dron199939@gmail.com>
2022-11-01 05:57:34 +0000

    dd

--- main.py
+++ main.py
@@ -36,8 +36,6 @@ class ksdf_all_packet_counters(Resource):
     def get(self):
         rpc = """<GetPacketCounters xmlns="urn:kaloom:faas:fabrics"></GetPacketCounters>"""
         return kaloom_netconf_rpc(rpc)['fabrics:Results']
-<<<<<<< HEAD
-=======
 
 @api.route('/api/ksdf/get_all_tp_oper')
 class ksdf_get_all_tp_oper(Resource):
@@ -45,7 +43,6 @@ class ksdf_get_all_tp_oper(Resource):
         rpc="""<get><filter type="subtree"><top xmlns="urn:kaloom:faas:fabrics"/><Fabric><FabricID>123</FabricID>
         <Node><Role></Role><TerminationPoint><OperState>UP</OperState></TerminationPoint></Node></Fabric></filter></get>""" 
         return kaloom_netconf_rpc(rpc)['data']['top']['fabrics:Fabric']
->>>>>>> e64132d01d6e8bb6dc8f86460aa948e37abbb302
 
 def kaloom_netconf_get_all():
     with manager.connect(host=klm_host, port=830, username=klm_user, password=klm_pwd, hostkey_verify=False) as m:
@@ -64,4 +61,3 @@ def kaloom_netconf_rpc(rpc):
 
 if __name__ == "__main__":
     app.run(debug=True, host='192.168.15.131', port=8000)
-    app.run(debug=True, host='0.0.0.0', port=8000)
