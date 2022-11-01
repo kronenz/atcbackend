e64132d01d6e8bb6dc8f86460aa948e37abbb302
warmtint0806@gmail.com <warmtint0806@gmail.com>
2022-11-01 11:04:27 +0900

    dd

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+#pip install flask 
+#pip install flask_cors
+#pip install ncclient
+#pip xmltodict
+
 from flask import Flask  # 서버 구현을 위한 Flask 객체 import
 from flask_restx import Api, Resource  # Api 구현을 위한 Api 객체 import
 from flask_cors import CORS, cross_origin
@@ -21,16 +26,23 @@ klm_host = "10.0.20.150"
 klm_user = "admin"
 klm_pwd = "Forwiz_SDF1"
 
-@api.route('/ksdf_get_all')
+@api.route('/api/ksdf/get_all')
 class ksdf_get_all(Resource):
     def get(self):
         return kaloom_netconf_get_all()
 
-@api.route('/ksdf_all_packet_counters')
+@api.route('/api/ksdf/all_packet_counters')
 class ksdf_all_packet_counters(Resource):
     def get(self):
         rpc = """<GetPacketCounters xmlns="urn:kaloom:faas:fabrics"></GetPacketCounters>"""
-        return kaloom_netconf_rpc(rpc)
+        return kaloom_netconf_rpc(rpc)['fabrics:Results']
+
+@api.route('/api/ksdf/get_all_tp_oper')
+class ksdf_get_all_tp_oper(Resource):
+    def get(self):
+        rpc="""<get><filter type="subtree"><top xmlns="urn:kaloom:faas:fabrics"/><Fabric><FabricID>123</FabricID>
+        <Node><Role></Role><TerminationPoint><OperState>UP</OperState></TerminationPoint></Node></Fabric></filter></get>""" 
+        return kaloom_netconf_rpc(rpc)['data']['top']['fabrics:Fabric']
 
 def kaloom_netconf_get_all():
     with manager.connect(host=klm_host, port=830, username=klm_user, password=klm_pwd, hostkey_verify=False) as m:
@@ -39,10 +51,13 @@ def kaloom_netconf_get_all():
         return parsed_data
 
 def kaloom_netconf_rpc(rpc):
-    with manager.connect(host=klm_host, port=830, username=klm_user, password=klm_pwd, hostkey_verify=False) as m:
-        c = m.rpc(to_ele(rpc)).xml
-        parsed_data = xmltodict.parse(c)['rpc-reply']
-        return parsed_data
+    try:
+        with manager.connect(host=klm_host, port=830, username=klm_user, password=klm_pwd, hostkey_verify=False) as m:
+            c = m.rpc(to_ele(rpc)).xml
+            parsed_data = xmltodict.parse(c)['rpc-reply']
+            return parsed_data
+    except: 
+        return {"error": "cannot process the request"}
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=7000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
