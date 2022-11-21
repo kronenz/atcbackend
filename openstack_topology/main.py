33795292cb3b01a89804a0b0cf9fe7e0b322d40a
kronenz <dron199939@gmail.com>
2022-11-21 06:07:08 +0000

    a

--- openstack_topology/main.py
+++ openstack_topology/main.py
@@ -113,5 +113,5 @@ class ostck_network_subnets_on_networks(Resource):
         return get_subnets_on_networks()
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=9000)
+    app.run(debug=True, host='192.168.15.131', port=9000)
 
